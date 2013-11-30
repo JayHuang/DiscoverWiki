ce9e1cda71c14d1512ceff16533cb54c13f4fee9
Jay Huang <jayhuang@example.com>
2013-11-30 01:16:46 -0800

    housekeeping

--- api/index.php
+++ api/index.php
@@ -3,10 +3,6 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-// echo ;
-// $PROJECT_ROOT = '/var/www/rethinkdb/';
-// require '../vendor/autoload.php';
-// use Slim\Slim;
 require_once("rdb/rdb.php");
 require_once("Slim/Slim.php");
 \Slim\Slim::registerAutoloader();
@@ -21,10 +17,6 @@ $app->get('/', 'home');
 
 $app->run();
 
-// public function __construct() {
-// 	$app->$conn = r\connect('localhost');
-// }
-
 function home() {
 	echo 'This is the API!';
 }
@@ -38,7 +30,6 @@ function storeArticle() {
 	$request = Slim::getInstance()->request();
 	$arr =  json_decode($request->getBody(), true);
 	$document = array('title' => $arr['title'], 'extract' => $arr['extract']);
-	// print_r($document);
 	$result = r\table('api')
 		->insert($document)
 		->run($conn);
@@ -49,7 +40,3 @@ function getConnection() {
 	$conn = r\connect('localhost', 28015, 'test');
 	return $conn;
 }
-
-// public function __destruct() {
-// 	$app->$conn = r\close();
-// }
