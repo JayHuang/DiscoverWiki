56e3128a043b921016b673d5be70e16d0ea93c7b
Jay Huang <jayhuang@example.com>
2013-12-01 03:16:23 -0800

    Added check to ensure multiple synchronous calls are not causing article limit to be exceeded

--- js/apiCtrl.js
+++ js/apiCtrl.js
@@ -3,6 +3,7 @@ var app = angular.module("app", ["ngAnimate"]);
 function apiCtrl($scope, $timeout, $http) {
   $scope.wikiArticles = [];
   var maxArticles = 10;
+  var isGrabbingArticles = false;
 
   $scope.wikiAPI = (function(){
     return {
@@ -32,12 +33,16 @@ function apiCtrl($scope, $timeout, $http) {
 
   $scope.fireAPICalls = function() {
     if($scope.wikiArticles.length < maxArticles) {
+      if(isGrabbingArticles)
+        return;
+      isGrabbingArticles = true;
       $scope.wikiAPI.getRandomArticle().success(function(data) {
         angular.forEach(data.query.pages, function(page, key) {
           $scope.wikiAPI.getURLforArticle(page.pageid).success(function(url){
             angular.forEach(url.query.pages, function(urlpage, urlkey){
               page.url = urlpage.fullurl;
               $scope.wikiArticles.push(page);
+              isGrabbingArticles = false;
               // $scope.serverAPI.storeArticle(page);
               $timeout($scope.fireAPICalls, 0);
             });
