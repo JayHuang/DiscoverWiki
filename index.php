675ead74e7e60dc57fc19dceafa5a1ac3c92fed2
Jay Huang <jayhuang@example.com>
2013-11-30 21:41:14 -0800

    Styling updates + deletion functionality

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html ng-app="app">
+<html>
 <head>
   <title>Discover Wikipedia!</title>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -8,17 +8,16 @@
   <script src="js/apiCtrl.js"></script>
   <link rel="stylesheet" href="css/main.css" type="text/css"/>
 </head>
-<body ng-controller="apiCtrl" ng-init="fireAPICalls();">
+<body ng-app="app" ng-controller="apiCtrl" ng-init="fireAPICalls();">
   <div class="random-articles">
     <input type="text" placeholder="Look for matching content" ng-model="search">
-    <button class="btn">Search</button>
-    <span>{{wikiArticles.length}} article(s)</span>
-    <div class="article-listing" ng-animate="'animate'" ng-repeat="article in wikiArticles | filter:search">
-      <span>{{article.title}}</span>
-      <ul>
+    <span>{{filtered.length}} article(s)</span>
+    <div class="article-listing" ng-animate="'animate'" ng-repeat="article in filtered = (wikiArticles | filter:search)">
+      <span class="article-title">{{article.title}}</span><span ng-click="articles.removeArticle($index)" class="delete-article" title="Remove article">&#10006;</span>
+      <!-- <ul>
         <li ng-repeat="category in article.categories">{{category.title}}</li>
-      </ul>
-      {{article.extract}}
+      </ul> -->
+      <div class"article-extract">{{article.extract}}</div>
     </div>
   </div>
 </body>
--- js/apiCtrl.js
+++ js/apiCtrl.js
@@ -2,6 +2,7 @@ var app = angular.module("app", ["ngAnimate"]);
 
 function apiCtrl($scope, $timeout, $http) {
   $scope.wikiArticles = [];
+  var maxArticles = 10;
 
   $scope.wikiAPI = (function(){
     return {
@@ -20,16 +21,21 @@ function apiCtrl($scope, $timeout, $http) {
     return {
       storeArticle : function(data) {
         return $http.post("api/storeArticle", (data));
+      },
+
+      getVersion : function() {
+        $http.get("api/version").success(function(data){
+          console.log(data);
+        });
       }
     }
   })();
 
   $scope.fireAPICalls = function() {
-    if($scope.wikiArticles.length < 10) {
+    if($scope.wikiArticles.length < maxArticles) {
       $scope.wikiAPI.getRandomArticle().success(function(data) {
         angular.forEach(data.query.pages, function(page, key) {
           $scope.wikiArticles.push(page);
-          // console.log(page);
           // $scope.serverAPI.storeArticle(page);
         });
         $timeout($scope.fireAPICalls, 0);
@@ -37,61 +43,15 @@ function apiCtrl($scope, $timeout, $http) {
     }
   }
 
-  $scope.shuffleArticles = function() {
-    $scope.wikiArticles.reverse();
-  }
-
-  $scope.getVersion = function() {
-    return $http.get("api/version").success(function(data){
-      console.log(data);
-    });
-  }
+  $scope.articles = (function() {
+    return {
+      shuffleArticles : function() {
+        $scope.wikiArticles.reverse();
+      },
+      removeArticle : function(index) {
+        $scope.wikiArticles.splice(index, 1);
+        $scope.fireAPICalls();
+      }   
+    }
+  })();
 }  
-
-  // "use strict";
-  // window.wikiData;
-  // var wikiAPI = (function(){
-  //   var self = this;
-  //   return {
-  //     getRandomArticle : function() {
-  //         return $.getJSON("http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exintro=&format=json&callback=?", function (data) {
-  //       });
-  //     }
-  //   }
-  // })();
-
-  // var serverAPI = (function() {
-  //   return {
-  //     storeArticle : function(data) {
-  //       return $.ajax({
-  //         url: "api/storeArticle",
-  //         type: "post",
-  //         data: JSON.stringify(data),
-  //         contentType: 'application/json; charset=utf-8',
-  //         dataType: 'json'
-  //       });
-  //     }
-  //   }
-  // })();
-
-  // function fireAPICalls() {
-  //   wikiAPI.getRandomArticle().done(function(data) {
-  //     for(var id in data.query.pages) {
-  //       wikiAPI.data = data.query.pages[id];
-  //     }
-  //     window.data = wikiAPI.data;
-  //     console.log(wikiAPI.data);
-  //     serverAPI.storeArticle(wikiAPI.data).done(function(data){
-  //     });
-  //     // setTimeout(fireAPICalls, 0);
-  //   });
-  // }
-
-  // function getVersion() {
-  //   return $.ajax({
-  //     url: "api/version",
-  //     type: "get"
-  //   }).done(function(data){
-  //     console.log(data);
-  //   });
-  // }
