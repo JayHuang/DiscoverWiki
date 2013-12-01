512988fff8fb81b53c6632ee8cf629b4317b715f
Jay Huang <jayhuang@example.com>
2013-11-30 22:44:47 -0800

    Styling changes, added normalize and grid systems

--- index.php
+++ index.php
@@ -6,14 +6,18 @@
   <script src="angular.js/build/angular-resource.min.js"></script>
   <script src="angular.js/build/angular-animate.min.js"></script>
   <script src="js/apiCtrl.js"></script>
-  <link rel="stylesheet" href="css/main.css" type="text/css"/>
+  <link rel="stylesheet" href="css/normalize-2.1.3.min.css">
+  <link rel="stylesheet" href="css/pure-0.3.0-grids.min.css">
+  <link rel="stylesheet" href="css/main.css">
 </head>
 <body ng-app="app" ng-controller="apiCtrl" ng-init="fireAPICalls();">
-  <div class="random-articles">
-    <input type="text" placeholder="Look for matching content" ng-model="search">
-    <span>{{filtered.length}} article(s)</span>
+  <div id="articles-listing">
+    <div id="base" class="pure-g-r">
+      <input class="pure-u-4-5" type="text" id="article-filter" placeholder="Filter articles" ng-model="search">
+      <span class="pure-u-1-5" id="article-count">{{filtered.length}} article(s)</span>
+    </div>
     <div class="article-listing" ng-animate="'animate'" ng-repeat="article in filtered = (wikiArticles | filter:search)">
-      <span class="article-title">{{article.title}}</span><span ng-click="articles.removeArticle($index)" class="delete-article" title="Remove article">&#10006;</span>
+      <span class="article-title">{{article.title}}</span><a ng-click="articles.removeArticle($index)" class="delete-article" title="Remove article">&#10006;</a>
       <!-- <ul>
         <li ng-repeat="category in article.categories">{{category.title}}</li>
       </ul> -->
