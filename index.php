a6abfc05c5b3c6ddead84fdb55a9646721a57568
Jay Huang <jayhuang@example.com>
2013-12-01 21:14:53 -0800

    Added favicon

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <title>Discover Wikipedia!</title>
+  <link rel="shortcut icon" href="img/favicon.ico" />
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
   <script src="angular.js/build/angular.min.js"></script>
   <script src="angular.js/build/angular-resource.min.js"></script>
