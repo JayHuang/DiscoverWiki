b772f808504f60d645c5b5b61782d3cd9da0fd75
Jay Huang <jayhuang@example.com>
2013-12-01 21:26:42 -0800

    Updated domain name in share title

--- templates/social-share.php
+++ templates/social-share.php
@@ -1,5 +1,5 @@
 <div ng-click="socialShare = !socialShare" class="addthis_toolbox addthis_default_style addthis_16x16_style" 
-addthis:title="{{article.title}} via Discover Wikipedia!" addthis:description="{{article.extract}}" addthis:url="{{article.url}}">
+addthis:title="{{article.title}} via discoverwiki.com" addthis:description="{{article.extract}}" addthis:url="{{article.url}}">
   <a class="addthis_button_facebook"></a>
   <a class="addthis_button_twitter"></a>
   <a class="addthis_button_google_plusone_share"></a>
