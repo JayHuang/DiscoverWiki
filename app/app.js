a46f8e9b79aef3e67eb2c444cec703983e0ec177
wilsonbalderrama <wilson.balderrama@gmail.com>
2014-02-18 16:55:41 -0400

    Trying to fix twitter title issue

--- app/app.js
+++ app/app.js
@@ -28,25 +28,29 @@ var app = angular.module("app", [
  *     ...       ^
  *   </div>
  */
-app.directive('addthisToolbox', function() {
+app.directive('addthisToolbox', ['$timeout', function($timeout) {
     return {
         restrict: 'A',
         transclude: true,
         replace: true,
         template: '<div ng-transclude></div>',
         link: function ($scope, element, attrs) {
-            // Dynamically init for performance reason
-            // Safe for multiple calls, only first call will be processed (loaded css/images, popup injected)
-            // http://support.addthis.com/customer/portal/articles/381263-addthis-client-api#configuration-url
-            // http://support.addthis.com/customer/portal/articles/381221-optimizing-addthis-performance
-            addthis.init();
-            // Ajax load (bind events)
-            // http://support.addthis.com/customer/portal/articles/381263-addthis-client-api#rendering-js-toolbox
-            // http://support.addthis.com/customer/portal/questions/548551-help-on-call-back-using-ajax-i-lose-share-buttons
-            addthis.toolbox($(element).get());
+              $timeout(function(){
+              // Dynamically init for performance reason
+              // Safe for multiple calls, only first call will be processed (loaded css/images, popup injected)
+              // http://support.addthis.com/customer/portal/articles/381263-addthis-client-api#configuration-url
+              // http://support.addthis.com/customer/portal/articles/381221-optimizing-addthis-performance
+              addthis.init();
+              // Ajax load (bind events)
+              // http://support.addthis.com/customer/portal/articles/381263-addthis-client-api#rendering-js-toolbox
+              // http://support.addthis.com/customer/portal/questions/548551-help-on-call-back-using-ajax-i-lose-share-buttons
+              addthis.toolbox($(element), {}, {
+                title: attrs.title
+              });  
+            });
         }
     }
-});
+}]);
 
 app.directive("ngTap", function() {
   return function($scope, $element, $attributes) {
